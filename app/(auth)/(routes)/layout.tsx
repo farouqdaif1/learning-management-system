171cfda9507b410454dab23e54f776c430cf8190
farouqdaif1 <daiffarouq@gmail.com>
2024-02-28 16:08:48 +0200

    Add Authintication with Clerk

---
+++ app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LayoutAuth = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full flex justify-center items-center">{children}</div>
+  );
+};
+
+export default LayoutAuth;
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
-      <p> protected page </p>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
