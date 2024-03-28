45b0f1d7caa22162cedf87bc3d662638ff1ce428
farouqdaif1 <daiffarouq@gmail.com>
2024-03-29 01:06:23 +0200

    Add mux setup

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,7 +1,11 @@
-import { db } from "@/lib/db";
+import Mux from "@mux/mux-node";
 import { auth } from "@clerk/nextjs"
 import { NextResponse } from "next/server"
-
+import { db } from "@/lib/db";
+const { video } = new Mux({
+    tokenId: process.env.MUX_TOKEN_ID,
+    tokenSecret: process.env.MUX_TOKEN_SECRET
+})
 export const PATCH = async (req: Request, { params }: { params: { courseId: string, chapterId: string } }) => {
     try {
         const { userId } = auth();
@@ -26,9 +30,35 @@ export const PATCH = async (req: Request, { params }: { params: { courseId: stri
                 ...values,
             }
         })
-        console.log(chapter)
+        if (values.videoUrl) {
+            const existingMuxData = await db.muxData.findFirst({
+                where: {
+                    chapterId: params.chapterId
+                }
+            })
+            if (existingMuxData) {
+                await video.assets.delete(existingMuxData.assetId);
 
-        //TODO HANDEL VIDEO UPLOAD
+                await db.muxData.delete({
+                    where: {
+                        id: existingMuxData.id
+                    }
+                });
+            }
+            const asset = await video.assets.create({
+                input: values.videoUrl,
+                playback_policy: ['public'],
+                test: false
+            });
+            await db.muxData.create({
+                data: {
+                    chapterId: params.chapterId,
+                    assetId: asset.id,
+                    playbackId: asset.playback_ids?.[0]?.id,
+                }
+            })
+        }
+
         return NextResponse.json(chapter)
     } catch (error) {
         console.log("[CHAPTER_POST]", error)
