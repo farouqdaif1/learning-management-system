3ce48cd0fb0f7231e4e690f3b78fe918cc33db35
farouqdaif1 <daiffarouq@gmail.com>
2024-04-02 18:53:12 +0200

    Add course data table

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -34,38 +34,44 @@ export const PATCH = async (req: Request, { params }: { params: { courseId: stri
 export const DELETE = async (req: Request, { params }: { params: { courseId: string } }) => {
     try {
         const { userId } = auth();
+
         if (!userId) {
             return new NextResponse("Unauthorized", { status: 401 });
         }
+
         const course = await db.course.findUnique({
             where: {
                 id: params.courseId,
-                userId: userId
-            }, include: {
+                userId: userId,
+            },
+            include: {
                 chapters: {
                     include: {
-                        muxData: true
+                        muxData: true,
                     }
                 }
             }
-        })
+        });
+
         if (!course) {
             return new NextResponse("Not found", { status: 404 });
         }
+
         for (const chapter of course.chapters) {
             if (chapter.muxData?.assetId) {
-                await video.assets.delete(chapter.muxData.assetId)
+                await video.assets.delete(chapter.muxData.assetId);
             }
         }
 
         const deletedCourse = await db.course.delete({
             where: {
                 id: params.courseId,
-            }
-        })
+            },
+        });
+
         return NextResponse.json(deletedCourse);
     } catch (error) {
-        console.log("[CourseIdDelete]", error)
-        return new NextResponse("Internal Error", { status: 500 })
+        console.log("[COURSE_ID_DELETE]", error);
+        return new NextResponse("Internal Error", { status: 500 });
     }
 }
--- scripts/seed.ts
+++ scripts/seed.ts
@@ -5,16 +5,10 @@ async function main() {
     try {
         await database.category.createMany({
             data: [
-                { name: "Web Development" },
-                { name: "Mobile Development" },
-                { name: "Data Science" },
-                { name: "Machine Learning" },
-                { name: "Artificial Intelligence" },
-                { name: "Cyber Security" },
-                { name: "Game Development" },
-                { name: "DevOps" },
-                { name: "Cloud Computing" },
-                { name: "UI/UX Design" },
+                { name: "الفرقة الاولي" },
+                { name: "الفرقة الثانية" },
+                { name: "الفرقة الثالثة" },
+                { name: "الفرقة الرابعه" },
             ]
         });
         console.log("seeded data base categories");
