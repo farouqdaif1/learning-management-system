1a5176578e1c4dbbeed3d5e9893f1e359cf2f02a
farouqdaif1 <daiffarouq@gmail.com>
2024-06-10 17:18:04 +0300

    Fix Video Form

--- app/(dashboard)/(routes)/(dashboad)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
+++ app/(dashboard)/(routes)/(dashboad)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
@@ -12,6 +12,7 @@ import { useRouter } from "next/navigation";
 import { Chapter, MuxData } from "@prisma/client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import MuxUploader from "@mux/mux-uploader-react";
 
 interface ChapterVideoFormProps {
   initialData: Chapter & { muxData?: MuxData | null };
@@ -30,40 +31,52 @@ export const ChapterVideoForm = ({
 }: ChapterVideoFormProps) => {
   const [isEditing, setIsEditing] = useState(false);
   const [loading, setLoading] = useState(false);
-  const toggleEdit = () => setIsEditing((current) => !current);
-
-  const router = useRouter();
-  const onUpload = async () => {
-    try {
-      setLoading(true);
-      const fileInput = document.getElementById(
-        "file-input"
-      ) as HTMLInputElement;
-
-      if (!fileInput.files || !fileInput.files[0]) {
-        throw new Error("لم يتم اختيار ملف الفيديو");
-      }
-      const data = await axios.get(
-        `/api/courses/${courseId}/chapters/${chapterId}`
-      );
-
-      const getUploadUrl = data.data.url;
-      const upload = UpChunk.createUpload({
-        endpoint: getUploadUrl,
-        file: fileInput.files[0],
-        chunkSize: 5120, // Uploads the file in ~5mb chunks
-      });
-      toggleEdit();
-    } catch (error) {
-      toast.error((error as Error).message);
-    } finally {
-      setTimeout(() => {
-        setLoading(true);
-        router.refresh();
-      }, 5000);
-      setLoading(false);
-    }
+  const [dataUrl, setDataUrl] = useState("");
+  const toggleEdit = async () => {
+    setIsEditing((current) => !current);
+    const data = await axios.get(
+      `/api/courses/${courseId}/chapters/${chapterId}`
+    );
+    setDataUrl(data.data.url);
   };
+  const router = useRouter();
+  const onSuccess = () => {
+    router.refresh();
+
+    toast.success("Video uploaded successfully");
+    router.refresh();
+  };
+  // const onUpload = async () => {
+  //   try {
+  //     setLoading(true);
+  //     const fileInput = document.getElementById(
+  //       "file-input"
+  //     ) as HTMLInputElement;
+
+  //     if (!fileInput.files || !fileInput.files[0]) {
+  //       throw new Error("لم يتم اختيار ملف الفيديو");
+  //     }
+  //     const data = await axios.get(
+  //       `/api/courses/${courseId}/chapters/${chapterId}`
+  //     );
+
+  //     const getUploadUrl = data.data.url;
+  //     const upload = UpChunk.createUpload({
+  //       endpoint: getUploadUrl,
+  //       file: fileInput.files[0],
+  //       chunkSize: 5120, // Uploads the file in ~5mb chunks
+  //     });
+  //     toggleEdit();
+  //   } catch (error) {
+  //     toast.error((error as Error).message);
+  //   } finally {
+  //     setTimeout(() => {
+  //       setLoading(true);
+  //       router.refresh();
+  //     }, 5000);
+  //     setLoading(false);
+  //   }
+  // };
   return (
     <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-between">
@@ -96,15 +109,15 @@ export const ChapterVideoForm = ({
         ))}
       {isEditing && (
         <div>
-          {loading && (
+          {/* {loading && (
             <div className="bg-slate-200/55 absolute left-0 top-0 text-sm text-muted-foreground  w-full h-full flex items-center justify-center ">
               <div className="flex flex-row items-center justify-center text-xl">
                 <Loader2 className="h-5 w-5 animate-spin" />
                 Uploading...
               </div>
             </div>
-          )}
-          <div className="p-6">
+          )} */}
+          {/* <div className="p-6">
             <Input
               className="mb-10"
               type="file"
@@ -112,6 +125,9 @@ export const ChapterVideoForm = ({
               id="file-input"
             />
             <Button onClick={() => onUpload()}>Submit</Button>
+          </div> */}
+          <div>
+            <MuxUploader endpoint={dataUrl} onSuccess={() => onSuccess()} />
           </div>
           <div className="text-xs text-muted-foreground mt-4">
             Upload this chapter&apos;s Video
