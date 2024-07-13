d8871356159c9d3aad10257b2b23743d37112ef7
farouqdaif1 <daiffarouq@gmail.com>
2024-07-13 06:21:55 +0300

    Add fix Call us

--- app/(course)/courses/[courseId]/checkout/_components/call-us.tsx
+++ app/(course)/courses/[courseId]/checkout/_components/call-us.tsx
@@ -33,9 +33,17 @@ const CallUsForm = ({
         />
       </div>
       <div className="h-full flex flex-col justify-center align-center ">
-        <h1 className="text-center text-4xl">
-          +201021246070 : لشراء المسار الرجاء الاتصال علي
-        </h1>
+        <h1 className="text-center text-4xl">: لشراء المسار </h1>
+        <ul className="h-full flex flex-col justify-between  align-start text-xl">
+          <li className="text-center">
+            العنوان📍: ١٣ ش مصطفي اسماعيل تاني تقاطع من مطعم هريدي ( مجمع
+            الكليات ) برج الصفوة الدور الثاني- الازاريطة - الاسكندرية
+          </li>
+          <li className="text-center">واتس اب 📱01272731495</li>
+          <li className="text-center">تليفون ☎️ 03-4860400</li>
+          <li className="text-center">تليفون ☎️ 03-4867607</li>
+          <li className="text-center">وي باي 📱 01550029060</li>
+        </ul>
       </div>
     </div>
   );
